30acaed31aad3d316b1c2fa32f3b32575f6063a6
srinath <you@example.com>
2022-06-11 23:56:29 +0530

    src/main file not showing trying to make it show

---
+++ src/main/java/com/nt/test/GitConflictResolve.java
@@ -0,0 +1,15 @@
+package com.nt.test;
+
+public class GitConflictResolve {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		
+		System.out.println("its Multiplication");
+		System.out.println("Multiplication: "+a*b);
+
+		System.out.println("hi happy");
+	}
+
+}
