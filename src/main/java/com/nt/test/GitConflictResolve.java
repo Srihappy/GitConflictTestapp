2855e00845a4082b7191b3f1955dc7b78ba972d5
srinath <you@example.com>
2022-06-11 23:02:30 +0530

    original w/o change

---
+++ src/main/java/com/nt/test/GitConflictResolve.java
@@ -0,0 +1,14 @@
+package com.nt.test;
+
+public class GitConflictResolve {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=30;
+		
+		System.out.println("its addition");
+		System.out.println("Addition: "+a+b);
+
+	}
+
+}
